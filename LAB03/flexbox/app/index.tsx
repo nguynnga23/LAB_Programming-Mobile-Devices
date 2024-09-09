528e64e193fd11703725b0573b95873a532aa438
Nguyen Nga <nguyennga200x@gmail.com>
2024-09-09 17:24:43 +0700

    login-screen

--- LAB03/flexbox/app/index.tsx
+++ LAB03/flexbox/app/index.tsx
@@ -1,28 +1,53 @@
-import { Text, View, Image, StyleSheet } from "react-native";
-
+import { Text, View, Image, StyleSheet, Button } from "react-native";
+import Login from "@/components/login/login";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-          justifyContent: 'space-between'
-      </Text>
-      <Image source={{uri:"https://picsum.photos/200"}} style={{height:150, width:150}}/>
-      <Image source={{uri:"https://picsum.photos/200"}} style={{height:150, width:150}}/>
-      <Image source={{uri:"https://picsum.photos/200"}} style={{height:150, width:150}}/>
-
+      <View style={styles.logo}></View>
+      <View style={styles.slogan}>
+        <Text style={styles.title}>GROW <br/> YOUR BUSINESS</Text>
+      </View>
+      <View style={styles.slogan}>
+        <Text style={styles.subtitle}>We will help you to grow your business using online server</Text>
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button title="LOGIN" color="#E3C000"/>
+        <Button title="SIGN UP" color="#E3C000"/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
+    backgroundColor : "#00CCF9",
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  logo:{
+    width: 140,
+    height: 140,
+    borderWidth: 15,
+    borderRadius: 100,
+    
+  },
+  slogan:{
+    width: "80%",
   },
   title:{
-    fontSize: 20,
-    fontWeight: 500,
-  }
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: 700
+  },
+  subtitle:{
+    textAlign: "center",
+    fontSize: 15,
+    fontWeight: 700
+  },
+  buttonWrapper:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%"
+  },
 });
---
+++ LAB03/flexbox/components/login/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text, Button, Image } from "react-native" 
+const Login = () =>{
+    return (
+        <View>
+            <Text>Nga</Text>
+        </View>
+    )
+}
+
+
+export default Login
