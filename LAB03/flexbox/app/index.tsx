4de0300223bfa5e9d47cb41d99f490a5584b7094
Nguyen Nga <nguyennga200x@gmail.com>
2024-09-09 16:09:01 +0700

    justifyContent: 'flex-start'

---
+++ LAB03/flexbox/app/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View, Image, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+          justifyContent: 'flex-start'
+      </Text>
+      <Image source={{uri:"https://picsum.photos/200"}} style={{height:150, width:150}}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  title:{
+    fontSize: 20,
+    fontWeight: 500,
+  }
+});
