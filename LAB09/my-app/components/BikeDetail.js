67355c5c5670d7d47115d564a7ba0b18b5f014dd
Nguyen Nga <nguyennga200x@gmail.com>
2024-11-11 15:30:02 +0700

    Build UI

---
+++ LAB09/my-app/components/BikeDetail.js
@@ -0,0 +1,85 @@
+import {View, Text, StyleSheet, FlatList, Image, TouchableOpacity, ScrollView} from "react-native"
+export default function BikeDetail(){
+ return(
+   <ScrollView>
+    <View style={{
+      flex: 1,
+      margin: 15
+
+    }}> 
+      <View style={{
+         flex: 1,
+         backgroundColor: "#E941411A",
+         justifyContent: "center",
+         alignItems: "center",
+         borderRadius: 10,
+      }}>
+         <Image source={require("../assets/bione-removebg-preview.png")}/>
+      </View>
+      <View style={{
+         flex: 1,
+         // justifyContent: "space-between"
+      }}>
+         {/* Name & price */}
+         <View style={{
+         }}>
+            <Text style={{
+               fontSize: 35,
+               fontWeight: 500,
+               paddingVertical: 15
+            }}>Pina Mountain</Text>
+
+            <Text style={{
+               fontSize: 25,
+               color: "#00000096",
+               fontWeight: 600
+            }}>15% OFF I 350$ <Text style={{
+               color: "#000",
+               textDecorationLine: "line-through",
+               paddingLeft: 30
+            }}>449$</Text></Text>
+         </View>
+         {/* Description */}
+         <View style={{
+         }}>
+            <Text style={{
+               fontSize: 25,
+               fontWeight: 500,
+               marginVertical: 20
+            }}>Description</Text>
+            <Text style={{
+               fontSize: 22,
+               fontWeight: 400,
+               color: "#00000091",
+               marginVertical: 20
+
+            }}>It is a very important form of writing as we write almost everything in paragraphs, be it an answer, essay, story, emails, etc.</Text>
+         </View>
+         {/* Cart button */}
+         <View style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: 30
+         }}>
+            <Image source={require("../assets/akar-icons_heart.png")}/>
+            <TouchableOpacity style={{
+               backgroundColor: "#E94141",
+               height: 60,
+               borderRadius: 30,
+               justifyContent: "center",
+               width: "80%"
+            }}>
+               <Text style={{
+                  color: "#fff",
+                  fontSize: 25,
+                  textAlign: "center"
+               }}>Add to card</Text>
+            </TouchableOpacity>
+         </View>
+      </View>
+    </View>
+    </ScrollView>
+ )
+}
+
---
+++ LAB09/my-app/components/ListBikes.js
@@ -0,0 +1,176 @@
+import { useNavigation } from "@react-navigation/native"
+import {View, Text, StyleSheet, FlatList, Image, TouchableOpacity, ScrollView} from "react-native"
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+</style>
+const DATA = [
+    {
+        id: "1",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "2",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "3",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "4",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "5",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "6",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "7",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "8",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "9",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+    {
+        id: "10",
+        imgPath: require("../assets/bifour_small_-removebg-preview.png"),
+        name: "Pinarello",
+        price: "1800"
+    },
+]
+
+const Item = ({imgPath, name, price}) =>{
+    const navigation = useNavigation();
+    return(
+        <TouchableOpacity style={{
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#F7BA8326",
+            borderRadius: 10,
+            margin: 7,
+            width: 200,
+        }}
+        onPress={()=>navigation.navigate("BikeDetail")}
+        >
+            <Image style={{
+                position: "absolute",
+                top: 5,
+                left: 15
+            }} source={require("../assets/heart.png")}/>
+            <Image style={{
+                margin: 15
+            }} source = {imgPath}/>
+            <Text style={{
+                padding: 10,
+                fontSize: 20,
+                fontWeight: 400,
+                color: "#00000099"
+            }}>{name}</Text>
+            <Text style={{
+                  padding: 10,
+                  fontSize: 20,
+                  fontWeight: 400,
+                //   fontFamily: "Ubuntu"
+            }}>
+                <Image source={require("../assets/$.png")}/>
+                {price}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+export default function ListBikes(){
+        return (
+            <View style={{
+                flex: 1,
+                padding: 20
+            }}>
+                <View style={{
+                    flex: 1,
+                    justifyContent: "space-around",
+
+                }}>
+                    <View style={{
+
+                    }}>
+                        <Text style={{
+                            fontSize: 25,
+                            fontWeight: 700,
+                            color: "#E94141"
+                        }}>The world’s Best Bike</Text>
+                    </View>
+
+                    <View style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between"
+                    }}>
+                        <TouchableOpacity style={styles.buttonFilter}>
+                            <Text style={styles.labelButton}>All</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonFilter}>
+                            <Text style={styles.labelButton}>Roadbike</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonFilter}>
+                            <Text style={styles.labelButton}>Mountain</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                    <View style={{
+                        flex: 5,
+                    }}>
+                        <FlatList
+                        data={DATA}
+                        renderItem={({item}) => <Item imgPath={item.imgPath} name={item.name} price={item.price}/>}
+                        keyExtractor={item => item.id}
+                        numColumns={2}
+                        contentContainerStyle={{alignItems: "center"}}
+                        />
+                    </View>
+            </View>
+        )
+    
+}
+
+const styles = StyleSheet.create({
+    buttonFilter:{
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: "#E9414187",
+        width: 99,
+        height: 40,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    labelButton:{
+        fontSize: 20,
+        fontWeight: 400,
+        color: "#E94141"
+    }
+})
